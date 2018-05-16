7a9b2a3845e5e6ef6d767a9d988b5117cdf0eb33
cm17g13 <RU4real95@hotmail.com>
2018-05-16 15:51:07 +0100

    changed import.sql to add default showings

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO Movie(movie_id, OMDbCode, title, year) VALUES(14, 'tt1396484', 'It', '2017');
-INSERT INTO Movie(movie_id, OMDbCode, title, year) VALUES(21, 'tt3890160', 'Baby Driver', '2017');
-INSERT INTO Movie(movie_id, OMDbCode, title, year) VALUES(73, 'tt4154664', 'Captain Marvel', '2019');
+INSERT INTO Movie(movie_id, OMDbCode, title, year, showings) VALUES(14, 'tt1396484', 'It', '2017','1@2:30,2@4:00,1@7:30');
+INSERT INTO Movie(movie_id, OMDbCode, title, year, showings) VALUES(21, 'tt3890160', 'Baby Driver', '2017','4@3:30,7@8:00,1@10:30');
+INSERT INTO Movie(movie_id, OMDbCode, title, year, showings) VALUES(73, 'tt4154664', 'Captain Marvel', '2019', '3@2:30,5@4:30,5@7:30');
 INSERT INTO Movie(movie_id, OMDbCode, title, year) VALUES(98, 'tt0417148', 'Snakes on a Plane', '2006');
